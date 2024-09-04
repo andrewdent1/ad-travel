2958810058d006554433f65d0dbffc51a40baeb0
Andrew Dent <andrew.dent@bbrown.com>
2024-09-04 16:26:58 -0400

    responsive design and navbar

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ document.getElementById('contact-form')?.addEventListener('submit', function (ev
     alert('Thank you for your message! We will get back to you shortly.');
     this.reset();
 });
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
